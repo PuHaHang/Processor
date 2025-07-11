33c5376af483ed431b0b795c4b70f78459089c66
juhy0987 <juhy0987@naver.com>
2025-07-11 12:34:13 +0900

    [FIX]: InitEvaluator 클래스의 available_evaluators를 available_processors로 변경하여 일관성 유지

--- src/common/processor/evaluator/strategies/init_evaluator.py
+++ src/common/processor/evaluator/strategies/init_evaluator.py
@@ -5,11 +5,11 @@ from ..evaluator_strategy import EvaluatorStrategy
 
 
 class InitEvaluator(EvaluatorStrategy):
-    available_evaluators: list = [None]
+    available_processors: list = [None]
 
 
     def evaluate(self, payload: Payload) -> bool:
         return super()._evaluate(str(payload.__str__))
     
     def is_supported(self, payload: Payload) -> bool:
-        return payload.get_processor() in self.available_evaluators
+        return payload.get_processor() in self.available_processors
