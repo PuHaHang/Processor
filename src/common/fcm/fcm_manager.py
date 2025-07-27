630b37cf06619e0889b4b418963b9a8aab1011be
juhy0987 <juhy0987@naver.com>
2025-07-28 00:11:19 +0900

    [REFACTOR]: FCM 관리자에서 인증서 경로 수정 - Firebase Admin SDK 인증서 파일 경로를 업데이트하여 코드 유지보수성 향상

--- src/common/fcm/fcm_manager.py
+++ src/common/fcm/fcm_manager.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, messaging
 
-cred = credentials.Certificate("src/resources/keys/recipe-it-3a7e1-firebase-adminsdk-fbsvc-ca60eb0c11.json")
+cred = credentials.Certificate("src/resources/keys/recipe-it-fcm-admin.json")
 firebase_admin.initialize_app(cred)
 
 def send_notification(token: str, title: str, body: str, image_url: str):
