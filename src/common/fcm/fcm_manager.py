5a6d81463ff0b3aca5a237d2d9f30a17b282f8d3
juhy0987 <juhy0987@naver.com>
2025-07-28 00:44:02 +0900

    [FIX]: Firebase Admin SDK 초기화 조건 추가 - 이미 초기화된 경우 중복 초기화를 방지하여 안정성 향상

--- src/common/fcm/fcm_manager.py
+++ src/common/fcm/fcm_manager.py
@@ -2,7 +2,8 @@ import firebase_admin
 from firebase_admin import credentials, messaging
 
 cred = credentials.Certificate("src/resources/keys/recipe-it-fcm-admin.json")
-firebase_admin.initialize_app(cred)
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred)
 
 def send_notification(token: str, title: str, body: str, image_url: str):
     registration_token = token
