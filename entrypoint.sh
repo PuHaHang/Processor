3a8ea50c86611b67b1c73a4866487edd122da016
juhy0987 <juhy0987@naver.com>
2025-08-06 03:14:52 +0900

    [HOTFIX]: entrypoint.sh에서 yt-dlp 설치 명령을 curl에서 pip로 변경

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # curl -I -s -c /tmp/cookies.txt -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36" "https://www.youtube.com"
-yt-dlp --update-to master
+pip install --upgrade yt-dlp
 yt-dlp "https://youtube.com/shorts/z4K1xfOZ06o?si=9poYTpngskv-fAFt"
 
 exec python main.py
