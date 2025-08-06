ef7a3381fbf761f706dc9d03b05becda8c85c8d1
juhy0987 <juhy0987@naver.com>
2025-08-06 12:46:19 +0900

    [HOTFIX]: entrypoint.sh에서 AWS 관련 명령 제거 및 yt-dlp 다운로드 및 실행 방식 변경

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -e
 
-aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID --profile default
-aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY --profile default
-aws configure set region $AWS_REGION --profile default
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o yt-dlp
+chmod +x yt-dlp
 
-aws s3 cp s3://$AWS_S3_BUCKET_NAME/$COOKIES_S3_PATH cookies.txt --region $AWS_REGION --profile default
-yt-dlp --cookies cookies.txt "https://youtube.com/shorts/z4K1xfOZ06o?si=9poYTpngskv-fAFt"
+./yt-dlp "https://youtube.com/shorts/z4K1xfOZ06o?si=9poYTpngskv-fAFt"
 
 exec python main.py
