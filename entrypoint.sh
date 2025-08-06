ef9890cb6df027a5e128798c75d62cf7aaf440d5
juhy0987 <juhy0987@naver.com>
2025-08-06 13:48:06 +0900

    [HOTFIX]: entrypoint.sh에서 yt-dlp 실행 시 Referer 헤더의 공백 수정

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ chmod +x yt-dlp
 ./yt-dlp --add-header "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36" \
   --add-header "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp" \
   --add-header "Accept-Language: en-US,en;q=0.9" \
-  --add-header "Referer: https://www.youtube.com/" \ 
+  --add-header "Referer: https://www.youtube.com/" \
   "https://youtube.com/shorts/z4K1xfOZ06o?si=9poYTpngskv-fAFt"
 
 exec python main.py
