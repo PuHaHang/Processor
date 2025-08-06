59d50053d8e34dc9e835c33e4283f1ccfd2dfcc5
juhy0987 <juhy0987@naver.com>
2025-08-06 14:15:40 +0900

    [HOTFIX]: entrypoint.sh에서 yt-dlp 실행 시 geo-bypass 및 인증서 확인 생략 옵션 추가

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,8 @@ chmod +x yt-dlp
   --add-header "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp" \
   --add-header "Accept-Language: en-US,en;q=0.9" \
   --add-header "Referer: https://www.youtube.com/" \
+  --geo-bypass \
+  --no-check-certificate \
   "https://youtube.com/shorts/z4K1xfOZ06o?si=9poYTpngskv-fAFt"
 
 exec python main.py
