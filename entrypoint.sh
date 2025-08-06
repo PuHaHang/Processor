a85e5b996b7518de6a7569270307e573434322cf
juhy0987 <juhy0987@naver.com>
2025-08-06 12:16:19 +0900

    [HOTFIX]: entrypoint.sh에서 S3에서 쿠키 파일 다운로드 명령에서 --recursive 플래그 제거

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID --profile default
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY --profile default
 aws configure set region $AWS_REGION --profile default
 
-aws s3 cp s3://$AWS_S3_BUCKET_NAME/$COOKIES_S3_PATH cookies.txt --region $AWS_REGION --profile default --recursive
+aws s3 cp s3://$AWS_S3_BUCKET_NAME/$COOKIES_S3_PATH cookies.txt --region $AWS_REGION --profile default
 yt-dlp --cookies cookies.txt "https://youtube.com/shorts/z4K1xfOZ06o?si=9poYTpngskv-fAFt"
 
 exec python main.py
