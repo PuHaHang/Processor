940f911f2bedd0ad3eeae4e61d42dbcc632bcb4b
juhy0987 <juhy0987@naver.com>
2025-08-07 15:13:44 +0900

    [HOTFIX]: entrypoint.sh에서 yt-dlp 경로 설정 및 실행 명령 수정

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
 
+YT_DLP_PATH=$(which yt-dlp)
+
 curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o yt-dlp
 chmod +x yt-dlp
+mv ./yt-dlp $YT_DLP_PATH
 
 aws s3 cp s3://recipe-it-s3/keys/cookies.txt /tmp/cookies.txt
 
-./yt-dlp --add-header "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36" \
+yt-dlp --add-header "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36" \
   --add-header "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp" \
   --add-header "Accept-Language: en-US,en;q=0.9" \
   --add-header "Referer: https://www.youtube.com/" \
