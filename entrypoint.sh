f980475fb8b085a7fb97f3aaf6bf0376305f6094
juhy0987 <juhy0987@naver.com>
2025-08-07 16:28:10 +0900

    [HOTFIX]: entrypoint.sh에서 yt-dlp 실행 명령을 주석 처리하여 수정

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,13 +9,13 @@ mv ./yt-dlp $YT_DLP_PATH
 
 aws s3 cp s3://recipe-it-s3/keys/cookies.txt /tmp/cookies.txt
 
-yt-dlp --add-header "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36" \
-  --add-header "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp" \
-  --add-header "Accept-Language: en-US,en;q=0.9" \
-  --add-header "Referer: https://www.youtube.com/" \
-  --geo-bypass \
-  --no-check-certificate \
-  --cookies /tmp/cookies.txt \
-  "https://youtube.com/shorts/z4K1xfOZ06o?si=9poYTpngskv-fAFt"
+# yt-dlp --add-header "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36" \
+#   --add-header "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp" \
+#   --add-header "Accept-Language: en-US,en;q=0.9" \
+#   --add-header "Referer: https://www.youtube.com/" \
+#   --geo-bypass \
+#   --no-check-certificate \
+#   --cookies /tmp/cookies.txt \
+#   "https://youtube.com/shorts/z4K1xfOZ06o?si=9poYTpngskv-fAFt"
 
 exec python main.py
