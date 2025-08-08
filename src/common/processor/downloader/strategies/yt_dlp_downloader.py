2d1d377780213560bd88dea1990a69fd82f47154
juhy0987 <juhy0987@naver.com>
2025-08-08 14:15:47 +0900

    [HOTFIX]: yt_dlp_downloader.py에서 extractor_args에 youtube:player_client=android 추가

--- src/common/processor/downloader/strategies/yt_dlp_downloader.py
+++ src/common/processor/downloader/strategies/yt_dlp_downloader.py
@@ -253,6 +253,7 @@ class YtDlpDownloader (DownloaderStrategy):
             'geo_bypass': True,
             'no_check_certificate': True,
             # 'extractor_args': f'youtubepot-bgutilhttp:base_url={os.getenv("YOUTUBE_POT_HTTP_URL", "http://localhost:4416")}',
+            'extractor_args': "youtube:player_client=android",
             'headers': {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp',
@@ -324,6 +325,7 @@ class YtDlpDownloader (DownloaderStrategy):
             'geo_bypass': True,
             'no_check_certificate': True,
             # 'extractor_args': f'youtubepot-bgutilhttp:base_url={os.getenv("YOUTUBE_POT_HTTP_URL", "http://localhost:4416")}',
+            'extractor_args': "youtube:player_client=android",
             'headers': {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp',
@@ -373,6 +375,7 @@ class YtDlpDownloader (DownloaderStrategy):
             'geo_bypass': True,
             'no_check_certificate': True,
             # 'extractor_args': f'youtubepot-bgutilhttp:base_url={os.getenv("YOUTUBE_POT_HTTP_URL", "http://localhost:4416")}',
+            'extractor_args': "youtube:player_client=android",
             'headers': {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp',
